8d25d1ac812cce85370d1bafe7cdee65be327d79
Farhan Mohammed Mansoor <farhan.mansoordxb@gmail.com>
2025-04-13 08:23:05 +0400

    Deployment bug-fixing

--- server.js
+++ server.js
@@ -44,11 +44,6 @@ app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
-// Catch-all (after all other routes)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 // Mounting routers
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
